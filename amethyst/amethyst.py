1d7e10e4792804650e83f3dce7c7b5bcc5463d66
mogadishu000 <329400@uwr.edu.pl>
2023-10-07 18:30:20 +0200

    cleanup

--- amethyst/amethyst.py
+++ amethyst/amethyst.py
@@ -1,14 +1,12 @@
-from rdkit.Chem.AllChem import rdchem
 from rdkit.Chem.rdchem import Atom, RWMol, Mol
 from rdkit.Chem.rdmolops import SanitizeMol, AddHs, Cleanup, RemoveAllHs
-from rdkit.Chem.AllChem import MolFromSmiles, ReplaceSubstructs, MolToSmiles
-from rdkit.Chem.rdmolfiles import MolToPDBFile
+from rdkit.Chem.AllChem import MolFromSmiles, ReplaceSubstructs
 from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers, StereoEnumerationOptions
 from typing import List, Union
 
 def placeholder_atom_sub(core_mol: Mol, placeholder_atom: str, r_groups: Union[List[str], List[Mol]]) -> List[Mol]:
     mols = []
-    
+
     if type(r_groups[0]) == Mol:
         for i in r_groups:
             mod_mol = ReplaceSubstructs(core_mol, 
